9e68714b31c329064998725ac5be0b3209079b19
JoaoVitorDeOliveira <contatovitordeoliveira@gmail.com>
2021-01-27 22:38:14 -0300

    End Tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+#Do that in case you want to change the orderof the fields that appers
+#class QuestionAdmin(admin.ModelAdmin):
+#    fields = ['pub_date', 'question_text']
+
+#With FieldSets each field is separeted by a title
+#class QuestionAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None,               {'fields': ['question_text']}),
+#        ('Date information', {'fields': ['pub_date']}),
+#    ]
+
+#admin.site.register(Question, QuestionAdmin)
+
+
+#admin.site.register(Question)
